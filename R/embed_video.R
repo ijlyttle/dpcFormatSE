fbe184094b97c510706e1a30dee7d8d8d6e9ef2f
ijlyttle <ian.lyttle@schneiderdashelectric.com>
2014-08-26 21:35:49 -0500

    adds embed_video, roxygenize

---
+++ R/embed_video.R
@@ -0,0 +1,32 @@
+library(htmltools)
+
+#' embed_vimeo
+#'
+#' creates a centered div for embedding a vimeo
+#'
+#' @param vimeo_id character for vimeo_id
+#' 
+#' @return html code describing div
+#' 
+#' @import htmltools
+#' @export
+#' 
+embed_vimeo <- function(vimeo_id){
+  
+  url_vimeo <- 
+    paste0("https://player.vimeo.com/video/", vimeo_id)  
+
+  tags$div(
+    align = "center",
+    tags$iframe(
+      src = url_vimeo,
+      width = "500",
+      height = "281",
+      frameborder = 0,
+      webkitallowfullscreen = "",
+      mozallowfullscreen = "",
+      allowfullscreen = ""
+    )
+  )
+      
+}
