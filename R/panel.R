b2fe25c88ce31f066337f3d8d830d7a82d57a942
ijlyttle <ian.lyttle@schneiderdashelectric.com>
2015-01-27 11:33:59 -0600

    updates buttons, panels
    ready for bootstrap 3!

--- R/panel.R
+++ R/panel.R
@@ -6,7 +6,7 @@
 #' @param body_html  character, html for body
 #' @param class      character, name of bootstrap panel class
 #'
-#' @return html fragment describing a bootstrap well-panel 
+#' @return html fragment describing a bootstrap panel 
 #' 
 #' @export
 #' 
@@ -19,11 +19,11 @@ panel_html <- function(title = "", body_html = "", class = "panel panel-default"
       htmltools::tags$h3(
         `class` = "panel-title",
         title
-      ),
-      htmltools::div(
-        `class` = "panel-body",
-        body_html
       )
+    ),
+    htmltools::div(
+      `class` = "panel-body",
+      body_html
     )
   )
   
@@ -38,7 +38,7 @@ panel_html <- function(title = "", body_html = "", class = "panel panel-default"
 #' @param body_markdown  character, markdown for body
 #' @param class      character, name of bootstrap panel class
 #'
-#' @return html fragment describing a bootstrap well-panel
+#' @return html fragment describing a bootstrap panel
 #' 
 #' @export
 #' 
@@ -48,3 +48,39 @@ panel_markdown <- function(title = "", body_markdown = "", class = "panel panel-
   
   panel_html(title, body_html, class)
 }
+
+#' panel_markdown_section
+#'
+#' Wraps panel_markdown, setting the class of the panel
+#'
+#' @param title          character, title for panel
+#' @param body_markdown  character, markdown for body
+#'
+#' @return html fragment describing a bootstrap panel
+#' 
+#' @export
+#' 
+panel_markdown_section <- function(title = "", body_markdown = ""){
+  
+  body_html <- md2html_fragment(body_markdown)
+  
+  panel_html(title, body_html,class = "panel panel-primary")
+}
+
+#' panel_html_exercise
+#'
+#' Wraps panel_html, setting the class of the panel
+#'
+#' @param title          character, title for panel
+#' @param body_html      character, html for body
+#'
+#' @return html fragment describing a bootstrap panel
+#' 
+#' @export
+#' 
+panel_html_exercise <- function(title = "", body_html = "", class = "panel panel-info"){
+  
+  panel_html(title, body_html, class)
+
+}
+
