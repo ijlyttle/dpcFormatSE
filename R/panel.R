5e73610b6757cfd1415333f652def6af85088796
ijlyttle <ian.lyttle@schneiderdashelectric.com>
2015-06-13 12:16:45 -0500

    converting html panel functions

--- R/panel.R
+++ R/panel.R
@@ -2,15 +2,15 @@
 #'
 #' Creates a panel 
 #'
-#' @param title      character, title for panel
 #' @param body_html  character, html for body
+#' @param title      character, title for panel
 #' @param class      character, name of bootstrap panel class
 #'
 #' @return html fragment describing a bootstrap panel 
 #' 
 #' @export
 #' 
-panel_html <- function(title = "", body_html = "", class = "panel panel-default"){
+panel_html <- function(body_html = "", title = "", class = "panel panel-default"){
   
   htmltools::tags$div(
     `class` = class,
@@ -44,9 +44,9 @@ panel_html <- function(title = "", body_html = "", class = "panel panel-default"
 #' 
 panel_markdown <- function(title = "", body_markdown = "", class = "panel panel-default"){
   
-  body_html <- md2html_fragment(body_markdown)
+  body_html <- dpcFormatSE::md2html_fragment(body_markdown)
   
-  panel_html(title, body_html, class)
+  dpcFormatSE::panel_html(body_html, title, class)
 }
 
 #' panel_markdown_section
@@ -64,7 +64,7 @@ panel_markdown_section <- function(title = "", body_markdown = ""){
   
   body_html <- md2html_fragment(body_markdown)
   
-  panel_html(title, body_html,class = "panel panel-primary")
+  dpcFormatSE::panel_html(body_html, title,  class = "panel panel-primary")
 }
 
 #' panel_html_exercise
@@ -80,7 +80,61 @@ panel_markdown_section <- function(title = "", body_markdown = ""){
 #' 
 panel_html_exercise <- function(title = "", body_html = "", class = "panel panel-info"){
   
-  panel_html(title, body_html, class)
+  dpcFormatSE::panel_html(body_html, title, class)
 
 }
+
+
+#' panel_exercise
+#' 
+#' builds an html panel, with a possibility for modal/modal botton at bottom 
+#' 
+#' At the bottom of the panel, there will be a button to activate a modal.
+#' 
+#' @param content_panel   character, markdown or html to use as content for panel ("")
+#' @param title_panel     character, title to display for panel ("Exercise")
+#' @param id_modal        character, unique within page, to identify the modal panel 
+#' @param content_modal   character, markdown or html to use as content for modal ("")
+#' @param title_modal     character, title to display for modal ("Answer")
+#' 
+#' @return html for panel and modal
+#' 
+panel_exercise <- 
+  function(content_panel = "", title_panel = "Exercise",
+           id_modal, content_modal = "", title_modal = "Answer"){
+    
+    # assemble some html, using whiskers {{}} for placeholders
+    html_raw <- 
+      tags$html(
+        markdown::markdownToHTML(text = content_modal, fragment.only = TRUE),
+        tags$div(
+          align = "center",
+          actionButton("{{id}}_btn", label = title_modal, class = "btn btn-info") 
+        ),
+        bsModal(
+          id = "{{id}}_modal", 
+          title = title_modal,
+          trigger = "{{id}}_btn",
+          size = "medium",
+          markdown::markdownToHTML(text = content_modal, fragment.only = TRUE)
+        ),
+        # some javascript to tell the panel to reload when it is hidden
+        # the effect of this is to stop videos from playing when the 
+        # modal window is closed.
+        #
+        # http://stackoverflow.com/questions/13799377/twitter-bootstrap-modal-stop-youtube-video
+        #
+        tags$script(
+          "$('#{{id}}_modal').on('hidden.bs.modal', function (e) {
+             $('#{{id}}_modal iframe').attr('src', $('#{{id}}_modal iframe').attr('src'));
+           });"
+        )
+      )
+  
+    # use whisker to interpolate {{id}}
+    html_interp <- whisker::whisker.render(html_raw, data = list(id = id_modal)) 
+    
+    # build the panel
+    dpcFormatSE::panel_html(html_div_proc, title = title_panel, class = "panel panel-info")
+  }
 
