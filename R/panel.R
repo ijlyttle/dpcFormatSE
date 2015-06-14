651a11e738929803a85743b37477c02bae34b744
ijlyttle <ian.lyttle@schneiderdashelectric.com>
2015-06-13 19:38:53 -0500

    includes panel_section

--- R/panel.R
+++ R/panel.R
@@ -67,6 +67,9 @@ panel_markdown_section <- function(title = "", body_markdown = ""){
   dpcFormatSE::panel_html(body_html, title,  class = "panel panel-primary")
 }
 
+
+
+
 #' panel_html_exercise
 #'
 #' Wraps panel_html, setting the class of the panel
@@ -84,6 +87,51 @@ panel_html_exercise <- function(title = "", body_html = "", class = "panel panel
 
 }
 
+js_reload_modal <- function(id){
+  # some javascript to tell the panel to reload when it is hidden
+  # the effect of this is to stop videos from playing when the 
+  # modal window is closed.
+  #
+  # http://stackoverflow.com/questions/13799377/twitter-bootstrap-modal-stop-youtube-video
+  #
+  script_template <-
+    "$('#{{id}}_modal').on('hidden.bs.modal', function (e) {
+      $('#{{id}}_modal iframe').attr('src', $('#{{id}}_modal iframe').attr('src'));
+    });"   
+  
+  script_interp <- 
+    htmltools::HTML(whisker::whisker.render(script_template, list(id = id)))
+ 
+  script_interp 
+}
+
+#' panel_section
+#' 
+#' builds an html panel, with a possibility for modal/modal botton at bottom 
+#' 
+#' At the bottom of the panel, there will be a button to activate a modal.
+#' 
+#' @param content_panel   character, markdown or html to use as content for panel ("")
+#' @param title_panel     character, title to display for panel ("")
+#' 
+#' @return html for panel 
+#' @export
+#' 
+panel_section <- 
+  function(content_panel = "", title_panel = ""){
+
+  # convert content to HTML
+  html_panel <- 
+    htmltools::HTML(
+      markdown::markdownToHTML(text = as.character(content_panel), fragment.only = TRUE)
+    )   
+  
+  html_content <- htmltools::tags$div(html_panel)
+  
+  # build the panel
+  dpcFormatSE::panel_html(html_content, title = title_panel, class = "panel panel-primary")  
+        
+}
 
 #' panel_exercise
 #' 
@@ -103,39 +151,43 @@ panel_html_exercise <- function(title = "", body_html = "", class = "panel panel
 panel_exercise <- 
   function(content_panel = "", title_panel = "Exercise",
            id_modal, content_modal = "", title_modal = "Answer"){
-    
-    # assemble some html, using whiskers {{}} for placeholders
-    html_raw <- 
-      tags$html(
-        markdown::markdownToHTML(text = "content_panel", fragment.only = TRUE),
-        tags$div(
-          align = "center",
-          actionButton("{{id}}_btn", label = title_modal, class = "btn btn-info") 
-        ),
-        bsModal(
-          id = "{{id}}_modal", 
-          title = title_modal,
-          trigger = "{{id}}_btn",
-          size = "medium",
-          markdown::markdownToHTML(text = "content_modal", fragment.only = TRUE)
-        ),
-        # some javascript to tell the panel to reload when it is hidden
-        # the effect of this is to stop videos from playing when the 
-        # modal window is closed.
-        #
-        # http://stackoverflow.com/questions/13799377/twitter-bootstrap-modal-stop-youtube-video
-        #
-        tags$script(
-          "$('#{{id}}_modal').on('hidden.bs.modal', function (e) {
-             $('#{{id}}_modal iframe').attr('src', $('#{{id}}_modal iframe').attr('src'));
-           });"
-        )
+
+    # convert content to HTML
+    html_panel <- 
+      htmltools::HTML(
+        markdown::markdownToHTML(text = as.character(content_panel), fragment.only = TRUE)
       )
-  
-    # use whisker to interpolate {{id}}
-    html_interp <- whisker::whisker.render(html_raw, data = list(id = id_modal)) 
     
+    html_modal <- 
+      htmltools::HTML(
+        markdown::markdownToHTML(text = as.character(content_modal), fragment.only = TRUE)
+      )
+
+    id_btn <- whisker::whisker.render("{{id}}_btn", list(id = id_modal))
+    id_modal_local <- whisker::whisker.render("{{id}}_modal", list(id = id_modal))
+    
+    # script to stop video when modal closed
+    script_modal <- js_reload_modal(id_modal)
+        
+    html_content <- 
+      htmltools::tags$div(
+        html_panel,
+        htmltools::tags$div(
+          align = "center",
+          shiny::actionButton(id_btn, label = title_modal, class = "btn btn-info") 
+        ),
+        shinyBS::bsModal(
+          id = id_modal_local, 
+          title = title_modal,
+          trigger = id_btn,
+          size = "medium",
+          html_modal
+        ),
+        htmltools::tags$script(script_modal)
+      )
+
     # build the panel
-    dpcFormatSE::panel_html(html_div_proc, title = title_panel, class = "panel panel-info")
+    dpcFormatSE::panel_html(html_content, title = title_panel, class = "panel panel-info")
+    
   }
 
